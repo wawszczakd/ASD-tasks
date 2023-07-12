5369d756fb6ff4e5c9f88d7340fa3239c2130cf0
Dominik Wawszczak <wawszczakd@gmail.com>
2023-07-12 22:53:51 +0200

    Changed to slower but nicer version

--- choinka.cpp
+++ choinka.cpp
@@ -1,19 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int read_int() {
-	char c = getchar_unlocked();
-	while (!('0' <= c && c <= '9')) {
-		c = getchar_unlocked();
-	}
-	int result = 0;
-	do {
-		result = result * 10 + (c - '0');
-		c = getchar_unlocked();
-	} while ('0' <= c && c <= '9');
-	return result;
-}
-
 constexpr int MAXN = 200'001;
 
 vector<int> children[MAXN];
@@ -28,110 +15,126 @@ void dfs(int u) {
 	post_order[u] = timer;
 }
 
-constexpr int BASE = 1 << 18;
-
-set<pair<int, int>> nodes[BASE << 1];
-
-void insert(int i, int color) {
-	for (i += BASE; i > 0; i >>= 1) {
-		int cnt = 1;
-		set<pair<int,int>>::iterator it = nodes[i].lower_bound(make_pair(color, 0));
-		if (it != nodes[i].end() && it->first == color) {
-			cnt += it->second;
-			nodes[i].erase(it);
-		}
-		nodes[i].insert(make_pair(color, cnt));
+struct segment_tree {
+private:
+	int base;
+	vector<set<pair<int, int>>> nodes;
+	
+public:
+	segment_tree() {
+		base = 0;
+		nodes.clear();
 	}
-}
-
-void erase(int i, int color) {
-	for (i += BASE; i > 0; i >>= 1) {
-		set<pair<int,int>>::iterator it = nodes[i].lower_bound(make_pair(color, 0));
-		int cnt = it->second - 1;
-		nodes[i].erase(it);
-		if (cnt > 0) {
+	
+	segment_tree(int n) {
+		base = 1;
+		while (base < n) {
+			base <<= 1;
+		}
+		nodes.clear();
+		nodes.resize(base << 1);
+	}
+	
+	void insert(int i, int color) {
+		for (i += base; i > 0; i >>= 1) {
+			int cnt = 1;
+			set<pair<int,int>>::iterator it = nodes[i].lower_bound(make_pair(color, 0));
+			if (it != nodes[i].end() && it->first == color) {
+				cnt += it->second;
+				nodes[i].erase(it);
+			}
 			nodes[i].insert(make_pair(color, cnt));
 		}
 	}
-}
-
-bool query(int l, int r) {
-	set<pair<int, int>> interval;
-	for (l += BASE, r += BASE + 1; l < r; l >>= 1, r >>= 1) {
-		if (l & 1) {
-			for (auto [color, cnt] : nodes[l]) {
-				set<pair<int, int>>::iterator it = interval.lower_bound(make_pair(color, 0));
-				if (it != interval.end() && it->first == color) {
-					cnt += it->second;
-					interval.erase(it);
-				}
-				interval.insert(make_pair(color, cnt));
-				if ((int) interval.size() > 2) {
-					return false;
-				}
-			}
-			l++;
-		}
-		if (r & 1) {
-			r--;
-			for (auto [color, cnt] : nodes[r]) {
-				set<pair<int, int>>::iterator it = interval.lower_bound(make_pair(color, 0));
-				if (it != interval.end() && it->first == color) {
-					cnt += it->second;
-					interval.erase(it);
-				}
-				interval.insert(make_pair(color, cnt));
-				if ((int) interval.size() > 2) {
-					return false;
-				}
+	
+	void erase(int i, int color) {
+		for (i += base; i > 0; i >>= 1) {
+			set<pair<int,int>>::iterator it = nodes[i].lower_bound(make_pair(color, 0));
+			int cnt = it->second - 1;
+			nodes[i].erase(it);
+			if (cnt > 0) {
+				nodes[i].insert(make_pair(color, cnt));
 			}
 		}
 	}
-	if ((int) interval.size() == 1) {
-		return true;
+	
+	bool query(int l, int r) {
+		set<pair<int, int>> interval;
+		for (l += base, r += base + 1; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) {
+				for (auto [color, cnt] : nodes[l]) {
+					set<pair<int, int>>::iterator it = interval.lower_bound(make_pair(color, 0));
+					if (it != interval.end() && it->first == color) {
+						cnt += it->second;
+						interval.erase(it);
+					}
+					interval.insert(make_pair(color, cnt));
+					if ((int) interval.size() > 2) {
+						return false;
+					}
+				}
+				l++;
+			}
+			if (r & 1) {
+				r--;
+				for (auto [color, cnt] : nodes[r]) {
+					set<pair<int, int>>::iterator it = interval.lower_bound(make_pair(color, 0));
+					if (it != interval.end() && it->first == color) {
+						cnt += it->second;
+						interval.erase(it);
+					}
+					interval.insert(make_pair(color, cnt));
+					if ((int) interval.size() > 2) {
+						return false;
+					}
+				}
+			}
+		}
+		if ((int) interval.size() == 1) {
+			return true;
+		}
+		else {
+			return (interval.begin()->second == 1 || next(interval.begin())->second == 1);
+		}
 	}
-	else {
-		return (interval.begin()->second == 1 || next(interval.begin())->second == 1);
-	}
-}
+};
 
 int main() {
-	int n = read_int(), q = read_int();
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n, q;
+	cin >> n >> q;
 	for (int u = 2; u <= n; u++) {
-		int parent = read_int();
+		int parent;
+		cin >> parent;
 		children[parent].emplace_back(u);
 	}
 	for (int u = 1; u <= n; u++) {
-		vertex_color[u] = read_int();
+		cin >> vertex_color[u];
 	}
 	
 	dfs(1);
 	
+	segment_tree tree(n + 1);
 	for (int u = 1; u <= n; u++) {
-		insert(pre_order[u], vertex_color[u]);
+		tree.insert(pre_order[u], vertex_color[u]);
 	}
 	
 	while (q--) {
-		char type = getchar_unlocked();
+		char type;
+		cin >> type;
 		if (type == '?') {
-			int u = read_int();
-			if (query(pre_order[u], post_order[u])) {
-				putchar_unlocked('T');
-				putchar_unlocked('A');
-				putchar_unlocked('K');
-			}
-			else {
-				putchar_unlocked('N');
-				putchar_unlocked('I');
-				putchar_unlocked('E');
-			}
-			putchar_unlocked('\n');
+			int u;
+			cin >> u;
+			cout << (tree.query(pre_order[u], post_order[u]) ? "TAK" : "NIE") << '\n';
 		}
 		else {
-			int u = read_int(), color = read_int();
-			erase(pre_order[u], vertex_color[u]);
+			int u, color;
+			cin >> u >> color;
+			tree.erase(pre_order[u], vertex_color[u]);
 			vertex_color[u] = color;
-			insert(pre_order[u], vertex_color[u]);
+			tree.insert(pre_order[u], vertex_color[u]);
 		}
 	}
 	
